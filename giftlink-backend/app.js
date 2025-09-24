f7d86246c805fa80e27f2591c1ac1d0b98261a0a
amjadajuz <ptamjadaju@gmail.com>
2025-09-24 02:03:50 -0400

    setup gift routes

--- giftlink-backend/app.js
+++ giftlink-backend/app.js
@@ -3,6 +3,8 @@ require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
 const pinoLogger = require('./logger');
+const giftRoutes=require('./routes/giftRoutes');
+const searchRoutes=require('./routes/searchRoutes')
 
 const connectToDatabase = require('./models/db');
 const {loadData} = require("./util/import-mongo/index");
@@ -34,12 +36,12 @@ const logger = require('./logger');
 
 app.use(pinoHttp({ logger }));
 
-// Use Routes
-// Gift API Task 2: add the giftRoutes to the server by using the app.use() method.
-//{{insert code here}}
+// Route files
+// Gift API Task 1: import the giftRoutes and store in a constant called giftroutes
+app.use("/gifts", giftRoutes);
 
-// Search API Task 2: add the searchRoutes to the server by using the app.use() method.
-//{{insert code here}}
+// Search API Task 1: import the searchRoutes and store in a constant called searchRoutes
+app.use("/search", searchRoutes);
 
 
 // Global Error Handler
