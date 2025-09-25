5b65a8d0f71440fe55f3791dbbbfaeaedaec7e88
amjadajuz <ptamjadaju@gmail.com>
2025-09-25 21:05:42 +0530

    Refactor giftRoutes.js to improve variable naming and ensure proper use of the collection.findOne method for retrieving gifts by ID.

--- giftlink-backend/routes/giftRoutes.js
+++ giftlink-backend/routes/giftRoutes.js
@@ -1,3 +1,4 @@
+/*jshint esversion: 8 */
 const express = require("express");
 const router = express.Router();
 const connectToDatabase = require("../models/db");
@@ -27,9 +28,10 @@ router.get("/:id", async (req, res) => {
     const db = await connectToDatabase();
 
     // Task 2: use the collection() method to retrieve the gift collection
-    const res = db.collection("gifts").find({ id: req.params.id });
+    const collection = db.collection("gifts");
 
-    // Task 3: Find a specific gift by ID using the collection.fineOne method and store in constant called gift
+    // Task 3: Find a specific gift by ID using the collection.findOne method and store in constant called gift
+    const gift = await collection.findOne({ id: req.params.id });
 
     if (!gift) {
       return res.status(404).send("Gift not found");
@@ -56,4 +58,3 @@ router.post("/", async (req, res, next) => {
 });
 
 module.exports = router;
-/*jshint esversion: 8 */
